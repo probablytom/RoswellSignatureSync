ae64adf3d9f0f16409f52ac7028037af53e24199
Tom Wallis <github@tom-wallis.com>
2015-07-17 12:03:39 +0100

    Added a signature manager for the local user

---
+++ LocalSignatureManager/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace LocalSignatureManager
+{
+    partial class SyncManagerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UserList = new System.Windows.Forms.ListBox();
+            this.ChangePassButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.AddUserButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // UserList
+            // 
+            this.UserList.FormattingEnabled = true;
+            this.UserList.Location = new System.Drawing.Point(12, 12);
+            this.UserList.Name = "UserList";
+            this.UserList.ScrollAlwaysVisible = true;
+            this.UserList.Size = new System.Drawing.Size(215, 56);
+            this.UserList.Sorted = true;
+            this.UserList.TabIndex = 2;
+            this.UserList.SelectedIndexChanged += new System.EventHandler(this.UserList_SelectedIndexChanged);
+            // 
+            // ChangePassButton
+            // 
+            this.ChangePassButton.Location = new System.Drawing.Point(233, 12);
+            this.ChangePassButton.Name = "ChangePassButton";
+            this.ChangePassButton.Size = new System.Drawing.Size(104, 23);
+            this.ChangePassButton.TabIndex = 3;
+            this.ChangePassButton.Text = "Change Details";
+            this.ChangePassButton.UseVisualStyleBackColor = true;
+            this.ChangePassButton.Click += new System.EventHandler(this.ChangePassButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(233, 81);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.TabIndex = 4;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Location = new System.Drawing.Point(314, 81);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(75, 23);
+            this.CloseButton.TabIndex = 5;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            // 
+            // AddUserButton
+            // 
+            this.AddUserButton.Location = new System.Drawing.Point(233, 41);
+            this.AddUserButton.Name = "AddUserButton";
+            this.AddUserButton.Size = new System.Drawing.Size(104, 23);
+            this.AddUserButton.TabIndex = 6;
+            this.AddUserButton.Text = "Add User";
+            this.AddUserButton.UseVisualStyleBackColor = true;
+            // 
+            // SyncManagerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 116);
+            this.Controls.Add(this.AddUserButton);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.ChangePassButton);
+            this.Controls.Add(this.UserList);
+            this.Name = "SyncManagerForm";
+            this.Text = "Signature Sync Manager Local Edition";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox UserList;
+        private System.Windows.Forms.Button ChangePassButton;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.Button AddUserButton;
+    }
+}
+
---
+++ LocalSignatureManager/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LocalSignatureManager
+{
+    public partial class SyncManagerForm : Form
+    {
+        public SyncManagerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangePassButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UserList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
